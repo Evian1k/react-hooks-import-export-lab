69177695feecfa0d806b0cbbcbcacb4d27e388f4
Evian1k <emmanuelevian@gmail.com>
2025-04-22 14:32:49 +0300

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 function Home() {
+  const username = "Jane";
+  const city = "Los Angeles";
+
   return (
     <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
+      <h1>{username} from {city}</h1>
     </div>
   );
 }
+
+export default Home;
+
